3c3b92b7fdd39d5984ccd6fad0223f5c709703af
Dylan Ables <dylanables@Dylans-MacBook-Pro.local>
2023-12-01 17:02:35 -0800

    Seperated frontend and backend

--- backend/models/Account.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const AccountSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    isAdmin: { type: Boolean, default: false },
-},{ timestamps: true });
-
-module.exports = mongoose.model("Account", AccountSchema);
--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "https://findpaddle-api.onrender.com/api/";
+const BASE_URL = "https://findpaddle-api./api/";
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NWYwY2JhYjNkODMwYTA1MGQxN2Y4MSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcwMDc5MDU0NiwiZXhwIjoxNzAxMDQ5NzQ2fQ.eIjkye3vntBrfXG2Dydpo7ST35-pLVYG00afCuQlHMM";
 
 export const publicRequest = axios.create({
