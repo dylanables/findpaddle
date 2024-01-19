1888145bbe213dc74dae9321f04ae06cf181811f
Dylan Ables <33791389+dylanables@users.noreply.github.com>
2024-01-18 22:59:24 -0800

    Removed userRequest method

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,15 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = "https://findpaddle-api.onrender.com/api/";
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
-? JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken
-: null;
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
-});
-
-export const userRequest = axios.create({
-    baseURL: BASE_URL,
-    headers: { token:`Bearer ${TOKEN}` },
 });
